8e4216083455283fafa752d1fe0b136a5505a4c7
Farnaz Zinnah <farnazsamia@gmail.com>
2026-09-14 16:40:28 -0400

    Set production portfolio domain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://farnaz-portfolio.vercel.app",
+  site: "https://farnazsamia.com",
   output: "static",
 
   integrations: [
